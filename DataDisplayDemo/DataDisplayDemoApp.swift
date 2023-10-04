60a2ffa5eb41635c1adf0c71ee466da2dc60be5a
Victor Ulloa <alejandroekas@gmail.com>
2023-10-04 12:48:36 -0400

    Initial Commit

---
+++ DataDisplayDemo/DataDisplayDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataDisplayDemoApp.swift
+//  DataDisplayDemo
+//
+//  Created by Victor Ulloa on 2023-10-04.
+//
+
+import SwiftUI
+
+@main
+struct DataDisplayDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
